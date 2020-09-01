69fc5fba952fe58cfd704f2103b57ff32c362634
Wayne Ngan <wayne.ngan@gmail.com>
2020-09-01 03:04:24 -0400

    Changed to correct docker account

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu
+FROM ubuntu:bionic
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV PASTA_ROOT=/var/www/pasta
--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 sudo docker run -d --rm \
     --name pasta-run \
     -p 80:80 \
-    wayn3927/pasta
+    wayne927/pasta
 
 #--mount type=bind,source="$(pwd)"/pasta/,target=/var/www/pasta/ \
 #--mount type=bind,source="$(pwd)"/pasta/files/,target=/var/www/pasta/files/ \
